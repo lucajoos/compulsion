f0b99e992f33fa55d5b4de4800df5dd465cd3368
Luca Joos <70900757+lucajoos@users.noreply.github.com>
2021-02-17 14:33:26 +0100

    Add options to .use() function

--- main.js
+++ main.js
@@ -10,27 +10,36 @@ module.exports = () => {
         modules = [modules];
       }
 
+      let q = (m, o) => {
+        let c = q => {
+          if(q?.name?.length > 0 && typeof q?.version === 'number') {
+            if(typeof r._[q.name] === 'object' ? q.version > r._[q.name]?.version : true) {
+              r._[q.name] = q;
+            }
+
+            t.fire(`ready-${q.name}`, q.version);
+            f++;
+
+            if(f === modules.length) {
+              t.fire('ready');
+            }
+          }
+        };
+
+        if(typeof m === 'function') {
+          m({
+            core: r,
+            build: c,
+            options: o || {}
+          });
+        }
+      };
+
       modules.forEach(module => {
         if(typeof module === 'function') {
-          let c = q => {
-            if(q?.name?.length > 0 && typeof q?.version === 'number') {
-              if(typeof r._[q.name] === 'object' ? q.version > r._[q.name]?.version : true) {
-                r._[q.name] = q;
-              }
-
-              t.fire(`ready-${q.name}`, q.version);
-              f++;
-
-              if(f === modules.length) {
-                t.fire('ready');
-              }
-            }
-          };
-
-          module({
-            core: r,
-            build: c
-          });
+          q(module);
+        } else if(typeof module === 'object' ? (typeof module?.module === 'function') : false) {
+          q(module?.module, module?.options);
         }
       });
 
